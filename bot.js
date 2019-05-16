b63e1aa14f2e3629b29bd60e21581702c2e29c15
rocketman1231 <50701122+rocketman1231@users.noreply.github.com>
2019-05-16 13:54:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,34 @@
- const discord = require('discord.js');
- const client = new Discord.Client();
- 
+ const Discord = require('discord.js');
+const { Client, Util } = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+
  
  client.on('ready', () => {
-     var x = client.channels.get("id channels");
+     var x = client.channels.get("578298778616987671");
      if (x) x.join();
  });
  
